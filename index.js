01219b89234b64267ea19874ec13b1b267825db4
borisinparis <mnkymgaa@gmail>
2024-12-02 16:42:14 +0800

    datatypes

---
+++ index.js
@@ -0,0 +1,8 @@
+// string => text types => "text"
+//numbers => text types => 2 3 4 50
+//boolen > true or false > 
+//object > { key : value}
+//array >[ index, index]
+// null or undefined => no value 
+// ***********  hamgiin ih ashiglagddag datatypes
+console.log("hello world");
